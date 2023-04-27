fa5181b23899b9ac291bcec50daffe45bba819f4
nrod42 <nigelrod94@gmail.com>
2023-04-26 21:22:45 -0400

    created login and register post requests. Login and register pages can now read and write to the database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+
+const salt = bcrypt.genSaltSync(10);
+const secret  = bcrypt.genSaltSync(10);
 
 /* GET home page. */
 router.get("/", function (req, res) {
@@ -14,7 +19,7 @@ router.post("/register", async function (req, res) {
     const userDoc = await User.create({
       email,
       username,
-      password,
+      password: bcrypt.hashSync(password, salt),
       date: new Date().toLocaleString("en-US"),
     });
     res.json(userDoc);
@@ -23,8 +28,19 @@ router.post("/register", async function (req, res) {
   }
 });
 
-router.post("/login", function (req, res) {
-  res.json("login");
+router.post("/login", async function (req, res) {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({username})
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  // res.json(userDoc);
+  res.json(passOk);
+  // if (passOk) {
+  //   //logged in
+
+  //   jwt.sign({username, id: userDoc._id}, secret )
+  // } else {
+  //   res.status(400).json('wrong credentials')
+  // }
 });
 
 module.exports = router;
