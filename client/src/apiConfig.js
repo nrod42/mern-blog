61eb5f1f1ca934187cdab41f2d5928fb3260728f
nrod42 <nigelrod94@gmail.com>
2023-05-29 16:12:03 -0400

    Changed API_URL for deployment

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
-// export const API_URL = "https://mernblog-api-2lf4.onrender.com";
+export const API_URL = "https://mernblog-api-2lf4.onrender.com";
 
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
 
