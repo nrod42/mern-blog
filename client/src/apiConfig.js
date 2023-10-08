ba0203ff80cf2f6371d3716e771b90e044ebcc61
nrod42 <nigelrod94@gmail.com>
2023-10-08 17:04:16 -0400

    updated api url

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,3 @@
-// export const API_URL = "https://mernblog-api-2lf4.onrender.com";
+export const API_URL = "https://mernblog-api-2lf4.onrender.com";
 
-export const API_URL = "http://localhost:8080";
-
+// export const API_URL = "http://localhost:8080";
